6c6471abbaab654dfba4194b842d67892924c553
Ritesh Pandey <rp@prioritycloud.in>
2025-06-08 11:54:17 +0530

    Enable public IP and use existing VPC resources

--- Automation/terraform/main.tf
+++ Automation/terraform/main.tf
@@ -1,15 +1,16 @@
 provider "aws" {
-region = var.region
+  region = var.region
 }
 
-# resource "aws_instance" "web" {
-#   ami           = var.ami_id
-#   instance_type = var.instance_type
-#   subnet_id     = var.subnet_id
-#   key_name      = var.key_name
-#   vpc_security_group_ids = [var.security_group_id]
+resource "aws_instance" "web" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+  key_name               = var.key_name
+  vpc_security_group_ids = [var.security_group_id]
+  associate_public_ip_address = true
 
-#   tags = {
-#     Name = "WebServer"
-#   }
-# }
+  tags = {
+    Name = "WebServer"
+  }
+}
