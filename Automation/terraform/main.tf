06e725a7b5cf611a4ba3bd2ba9bce7e3dd739659
Ritesh Pandey <rp@prioritycloud.in>
2025-06-08 11:45:09 +0530

    Remove EC2 instance from Terraform

--- Automation/terraform/main.tf
+++ Automation/terraform/main.tf
@@ -1,15 +1,15 @@
-provider "aws" {
-  region = var.region
-}
+# provider "aws" {
+#   region = var.region
+# }
 
-resource "aws_instance" "web" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  subnet_id     = var.subnet_id
-  key_name      = var.key_name
-  vpc_security_group_ids = [var.security_group_id]
+# resource "aws_instance" "web" {
+#   ami           = var.ami_id
+#   instance_type = var.instance_type
+#   subnet_id     = var.subnet_id
+#   key_name      = var.key_name
+#   vpc_security_group_ids = [var.security_group_id]
 
-  tags = {
-    Name = "WebServer"
-  }
-}
+#   tags = {
+#     Name = "WebServer"
+#   }
+# }
