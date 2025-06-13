0ff6a5d46e794a38b5c2d7485bffd7ce768fc094
Ritesh Pandey <rp@prioritycloud.in>
2025-06-13 12:36:48 +0530

    Use existing VPC, subnet and security group via variables

--- Automation/terraform/main.tf
+++ Automation/terraform/main.tf
@@ -2,15 +2,15 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_instance" "web" {
-  ami                    = var.ami_id
-  instance_type          = var.instance_type
-  subnet_id              = var.subnet_id
-  key_name               = var.key_name
-  vpc_security_group_ids = [var.security_group_id]
-  associate_public_ip_address = true
+# resource "aws_instance" "web" {
+#   ami                    = var.ami_id
+#   instance_type          = var.instance_type
+#   subnet_id              = var.subnet_id
+#   key_name               = var.key_name
+#   vpc_security_group_ids = [var.security_group_id]
+#   associate_public_ip_address = true
 
-  tags = {
-    Name = "WebServer"
-  }
-}
+#   tags = {
+#     Name = "WebServer"
+#   }
+# }
